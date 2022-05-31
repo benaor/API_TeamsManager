3bb3f8c9e5d164b9b0febd9ce4296a31c8507a4e
benaor <benjamingirard25@gmail.com>
2022-06-01 00:15:09 +0200

    remove debugger

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,8 @@
-console.log("Hello worl")
+import express from "express"
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get("/", (req, res) => res.send("Hello world"))
+
+app.listen(port, () => console.log(`Application running on port ${port}`))
