09d7010f951b9c5a2e87d25a2b7d9451629c15a5
benaor <benjamingirard25@gmail.com>
2022-06-01 00:26:18 +0200

    configure DB access

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,21 @@
 import express from "express"
+import { config } from "dotenv"
+import * as sqlite from "sqlite3"
+
+config()
 
 const app = express()
 const port = process.env.PORT || 3000
+const dbPath = process.env.DB_PATH || ""
+const db = new sqlite.Database(dbPath)
 
 app.get("/", (req, res) => res.send("Hello world"))
+
+app.get("/employee", (req, res) => {
+  db.all("SELECT * FROM Employee", [], (err, rows) => {
+    if (err) throw err
+    res.json(rows)
+  })
+})
 
 app.listen(port, () => console.log(`Application running on port ${port}`))
