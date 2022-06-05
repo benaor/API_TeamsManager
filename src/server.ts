2ecea04c2b749696899a90dc85a064b7035e38b4
benaor <benjamingirard25@gmail.com>
2022-06-05 11:27:00 +0200

    add body-parser, morgan and cors

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,21 @@ import express from "express"
 import { config } from "dotenv"
 import { routeCollection } from "./infrastructure/routeCollection"
 import "./controllers/employeeController"
+import bodyParser from "body-parser"
+import morgan from "morgan"
+import cors from "cors"
 
 config()
 
 const app = express()
 const port = process.env.PORT || 3000
-
 const router = express.Router()
+
 routeCollection.setupRouter(router)
+
 app.use(router)
+app.use(bodyParser.json())
+app.use(morgan("dev"))
+app.use(cors())
 
 app.listen(port, () => console.log(`Application running on port ${port}`))
