ef575d34915b00e66efa4709e49b04c950446492
benaor <benjamingirard25@gmail.com>
2022-06-06 12:12:15 +0200

    rewrite employeeController without Dependance inversion

--- src/controllers/employeeController.ts
+++ src/controllers/employeeController.ts
@@ -1,19 +1,21 @@
 import { Request, Response } from "express"
 import { Controller } from "../decorators/controllers"
-import * as sqlite from "sqlite3"
 import { get } from "../decorators/route"
+import { EmployeeRepository } from "../models/repositories/employeeRepository"
+import { Repository } from "../models/repositories/repository"
+import { Employee } from "../models/types/employee"
 
 @Controller()
 class EmployeeController {
-  @get()
-  getAll(req: Request, res: Response) {
-    const dbPath = process.env.DB_PATH || ""
-    const db = new sqlite.Database(dbPath)
+  private readonly employeeRepository: Repository<Employee>
 
-    db.all("SELECT * FROM Employee", [], (err, rows) => {
-      if (err) throw err
-      res.json(rows)
-    })
+  constructor() {
+    this.employeeRepository = new EmployeeRepository()
+  }
+
+  @get()
+  async getAll(req: Request, res: Response) {
+    const employees = await this.employeeRepository.getAll()
+    res.json(employees)
   }
 }
-
