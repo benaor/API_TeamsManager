7e4aa6ceb3a2165675bab5bd6f9b292a2702eebf
benaor <benjamingirard25@gmail.com>
2022-06-06 12:52:26 +0200

    create Dependency Decorator

---
+++ src/decorators/dependency.ts
@@ -0,0 +1,10 @@
+import { dependencyService } from "../infrastructure/dependencyService"
+
+const Dependency = (key: Symbol) => {
+  const decorator: ClassDecorator = (target: any) => {
+    dependencyService.add(key, target)
+  }
+  return decorator
+}
+
+export { Dependency }
